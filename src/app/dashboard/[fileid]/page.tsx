6fbf83654bd5a9eda196ce12d29b6ad63480ba00
Ilchenko Oleh <deckart212@gmail.com>
2023-10-17 00:17:01 +0300

    add displaying PDF files

--- src/app/dashboard/[fileid]/page.tsx
+++ src/app/dashboard/[fileid]/page.tsx
@@ -28,17 +28,17 @@ const Page = async ({ params }: PageProps) => {
   if (!file) notFound();
 
   return (
-    <div className='flex-1 justify-between flex-col h-[calc(100vh-3.5rem)]'>
-      <div className='mx-auto w-full max-w-7xl grow lg:flex xl:px-2'>
+    <div className='flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]'>
+      <div className='mmx-auto w-full max-w-8xl grow lg:flex xl:px-2'>
         {/* left section */}
         <div className='flex-1 xl:flex'>
           <div className='px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6'>
-            <PdfRenderer />
+            <PdfRenderer url={file.url} />
           </div>
         </div>
 
         {/* right side */}
-        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+        <div className='shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0'>
           <ChatWrapper />
         </div>
       </div>
